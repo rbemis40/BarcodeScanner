d756e5cde562259cdd00f2dfe9e202b702b2300d
Ryan Bemis <ryanbemis40@gmail.com>
2024-01-31 13:26:04 -0500

    Init Commit

---
+++ src/Barcode/Product.java
@@ -0,0 +1,22 @@
+package Barcode;
+
+public class Product {
+    long upc;
+    String sku;
+    int quantity;
+
+    public Product(long upc, String sku, int quantity) {
+        this.upc = upc;
+        this.sku = sku;
+        this.quantity = quantity;
+    }
+    public Product(long upc) {
+        this(upc, "", 1);
+    }
+
+    public Product(long upc, int quantity) {
+        this(upc, "", quantity);
+    }
+
+
+}
